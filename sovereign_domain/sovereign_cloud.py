42201273d7a55df4c80faa60a38298fdd882170d
vmagelo <51368918+vmagelo@users.noreply.github.com>
2023-01-30 17:00:00 +0100

    Update to remove unused variable
    And links to possible DefaultAzureCredential environment variables that can be used.

--- sovereign_domain/sovereign_cloud.py
+++ sovereign_domain/sovereign_cloud.py
@@ -3,10 +3,10 @@ from msrestazure.azure_cloud import AZURE_CHINA_CLOUD as CLOUD
 from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
 from azure.identity import DefaultAzureCredential, AzureAuthorityHosts
 
-# Assumes the subscription ID and tenant ID to use are in the AZURE_SUBSCRIPTION_ID and
-# AZURE_TENANT_ID environment variables
+# Set environment variable AZURE_SUBSCRIPTION_ID as well as environment variables
+# for DefaultAzureCredential. For combinations of environment variables, see
+# https://github.com/Azure/azure-sdk-for-python/tree/main/sdk/identity/azure-identity#environment-variables
 subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
-tenant_id = os.environ["AZURE_TENANT_ID"]
 
 # When using sovereign domains (that is, any cloud other than AZURE_PUBLIC_CLOUD),
 # you must use an authority with DefaultAzureCredential.
