b43858dd399b4c0e6e9dfc36c6e5e29712a4edac
vmagelo <v-gelorminom@microsoft.com>
2023-03-17 18:47:29 +0100

    Shorten examples to the way they were.

--- managed_disk/disk_from_blob.py
+++ managed_disk/disk_from_blob.py
@@ -1,16 +1,4 @@
-import os
-
-from azure.identity import AzureCliCredential
-from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.compute.models import DiskCreateOption
-
-# Acquire a credential object using CLI-based authentication.
-credential = AzureCliCredential()
-
-# Retrieve subscription ID from environment variable.
-subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
-
-compute_client = ComputeManagementClient(credential, subscription_id)
 
 poller = compute_client.disks.begin_create_or_update(
     'my_resource_group',
