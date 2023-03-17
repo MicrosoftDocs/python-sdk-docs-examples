93dce83b8932e141e35120702077fb6dda51c544
vmagelo <v-gelorminom@microsoft.com>
2023-03-17 19:03:00 +0100

    Small changes to bring samples in line with each other.

--- managed_disk/create_image_from_blob.py
+++ managed_disk/create_image_from_blob.py
@@ -7,7 +7,7 @@ async_create_image = compute_client.images.create_or_update(
             'os_disk': {
                 'os_type': 'Linux',
                 'os_state': "Generalized",
-                'blob_uri': 'https://bg09.blob.core.windows.net/vm-images/non-existent.vhd',
+                'blob_uri': 'https://<storage-account-name>.blob.core.windows.net/vm-images/test.vhd',
                 'caching': "ReadWrite",
             }
         }
