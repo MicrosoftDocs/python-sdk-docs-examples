ccf264037dc6f89df2fb116f0b437f2e34a928a3
vmagelo <v-gelorminom@microsoft.com>
2023-03-17 18:06:43 +0100

    Update examples.

--- managed_disk/disk_from_blob.py
+++ managed_disk/disk_from_blob.py
@@ -1,4 +1,16 @@
+import os
+
+from azure.identity import AzureCliCredential
+from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.compute.models import DiskCreateOption
+
+# Acquire a credential object using CLI-based authentication.
+credential = AzureCliCredential()
+
+# Retrieve subscription ID from environment variable.
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+
+compute_client = ComputeManagementClient(credential, subscription_id)
 
 poller = compute_client.disks.begin_create_or_update(
     'my_resource_group',
@@ -6,8 +18,9 @@ poller = compute_client.disks.begin_create_or_update(
     {
         'location': 'eastus',
         'creation_data': {
-            'create_option': DiskCreateOption.import_enum,
-            'source_uri': 'https://bg09.blob.core.windows.net/vm-images/non-existent.vhd'
+            'create_option': DiskCreateOption.IMPORT,
+            'storage_account_id': '/subscriptions/<subscription-id>/resourceGroups/<resource-group-name>/providers/Microsoft.Storage/storageAccounts/<storage-account-name>',
+            'source_uri': 'https://<storage-account-name>.blob.core.windows.net/vm-images/test.vhd'
         }
     }
 )
--- managed_disk/disk_image_from_blob.py
+++ managed_disk/disk_image_from_blob.py
@@ -1,18 +1,31 @@
-from azure.mgmt.compute.models import DiskCreateOption
+import os
+
+from azure.identity import AzureCliCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import OperatingSystemStateTypes, HyperVGeneration
+
+# Acquire a credential object using CLI-based authentication.
+credential = AzureCliCredential()
+
+# Retrieve subscription ID from environment variable.
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+
+compute_client = ComputeManagementClient(credential, subscription_id)
 
 poller = compute_client.images.begin_create_or_update(
     'my_resource_group',
-    'my_image_name',
+    'my_disk_name',
     {
         'location': 'eastus',
         'storage_profile': {
            'os_disk': {
               'os_type': 'Linux',
-              'os_state': "Generalized",
-              'blob_uri': 'https://bg09.blob.core.windows.net/vm-images/non-existent.vhd',
+              'os_state': OperatingSystemStateTypes.GENERALIZED,
+              'blob_uri': 'https://<storage-account-name>.blob.core.windows.net/vm-images/test.vhd',
               'caching': "ReadWrite",
-           }
-        }
+           },
+        },
+        'hyper_v_generation': HyperVGeneration.V2,
     }
 )
 image_resource = poller.result()
