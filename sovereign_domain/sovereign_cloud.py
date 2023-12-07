11a13073ce24015b9124b0e79d93f7d46ed5a1ca
JimacoMS4 <84105740+JimacoMS4@users.noreply.github.com>
2023-12-07 12:05:33 -0800

    update sovereign code snippets per Xiang Yan to address doc gh issue 1296

--- sovereign_domain/sovereign_cloud.py
+++ sovereign_domain/sovereign_cloud.py
@@ -1,7 +1,9 @@
 import os
-from msrestazure.azure_cloud import AZURE_CHINA_CLOUD as CLOUD
 from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
 from azure.identity import DefaultAzureCredential, AzureAuthorityHosts
+
+authority = AzureAuthorityHosts.AZURE_CHINA
+resource_manager = "https://management.chinacloudapi.cn"
 
 # Set environment variable AZURE_SUBSCRIPTION_ID as well as environment variables
 # for DefaultAzureCredential. For combinations of environment variables, see
@@ -10,14 +12,15 @@ subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
 # When using sovereign domains (that is, any cloud other than AZURE_PUBLIC_CLOUD),
 # you must use an authority with DefaultAzureCredential.
-credential = DefaultAzureCredential(authority=AzureAuthorityHosts.AZURE_CHINA)
+credential = DefaultAzureCredential(authority=authority)
 
 resource_client = ResourceManagementClient(
     credential, subscription_id,
-    base_url=CLOUD.endpoints.resource_manager,
-    credential_scopes=[CLOUD.endpoints.resource_manager + "/.default"])
+    base_url=resource_manager,
+    credential_scopes=[resource_manager + "/.default"])
 
 subscription_client = SubscriptionClient(
     credential,
-    base_url=CLOUD.endpoints.resource_manager,
-    credential_scopes=[CLOUD.endpoints.resource_manager + "/.default"])
+    base_url=resource_manager,
+    credential_scopes=[resource_manager + "/.default"])
+ 
